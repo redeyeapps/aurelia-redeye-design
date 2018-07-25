8dd841220bcbeb32989e12b7a32a5815960f643c
Nick Garner <garner104@gmail.com>
2018-07-25 10:44:21 +1000

    feat(spreadsheet): added change case and value fix WFMDEV-1924 (#14)

---
+++ src/value-converters/change-text-case.ts
@@ -0,0 +1,12 @@
+const changeCase = require('change-case');
+
+export class ChangeTextCaseValueConverter {
+  toView(str: string = '', ...types: string[]) {
+    if (!types.length || types[0] == undefined) return str;
+    let temp = str;
+    for (const type of types) {
+      temp = changeCase[type](temp);
+    }
+    return temp;
+  }
+}
